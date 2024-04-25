0101c582a92bf924b7165d2c5203b155fd371845
amine <a.sehlaoui96@gmail.com>
2024-04-25 15:19:27 -0700

    new file

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
-"""the Rotate 2D Matrix project.
-"""
+'''
+Module 0-rotate_2d_matrix.py Rotate 2D Matrix
+'''
+
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
-    """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-            r -= 1
+    ''' function to rotate 2D matrix 90 degrees
+    clockwise '''
+    # transpose matrix
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
+    # reverse transposed matrix
+    N = len(matrix)
+    for i in range(N // 2):
+        for j in range(N):
+            matrix[j][i], matrix[j][N - 1 - i] = (matrix[j][N - 1 - i],
+                                                  matrix[j][i])
